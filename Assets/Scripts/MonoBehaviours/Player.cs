2a9d4c0f15b38e53894dd660cc6370c89901451c
Stephan Pretorius <sjpretorius001@gmail.com>
2022-02-11 12:25:16 +0200

    First commit

---
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player : Character
+{
+
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+        
+    }
+
+}
